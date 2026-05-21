ca0bd31c6ae56f27a6b94a655bbeb18441e6c4d5
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-05-21 01:21:12 -0300

    Clear page and layout | new globals.css

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
