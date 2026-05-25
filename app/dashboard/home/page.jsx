d2965fa8e6eed30242a9ad215b8f837908ca00af
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-05-25 03:52:00 -0300

    Delete DataTable

--- app/dashboard/home/page.jsx
+++ app/dashboard/home/page.jsx
@@ -1,10 +1,8 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import data from "./data.json";
 import ChartVerticalBar from "@/components/chartVerticalBar";
 import { ChartPieLegend } from "@/components/chartPieLegend";
 
@@ -30,7 +28,6 @@ export default function Page() {
                 </div>
               </div>
               <div className="px-4 lg:px-6"></div>
-              <DataTable data={data} />
             </div>
           </div>
         </div>
