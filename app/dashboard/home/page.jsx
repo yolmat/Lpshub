4aee363f222bd8c40909171672573cc44f663fb5
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-05-25 02:28:19 -0300

    ComponentchatVerticalBar concluid

--- app/dashboard/home/page.jsx
+++ app/dashboard/home/page.jsx
@@ -1,21 +1,20 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
-import { SiteHeader } from "@/components/site-header"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import data from "./data.json"
+import data from "./data.json";
+import ChartVerticalBar from "@/components/chartVerticalBar";
 
 export default function Page() {
   return (
     <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)"
-        }
-      }>
+      style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 12)",
+      }}
+    >
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
@@ -24,7 +23,7 @@ export default function Page() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                <ChartVerticalBar />
               </div>
               <DataTable data={data} />
             </div>
---
+++ components/chartVerticalBar.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart";
+import { TrendingUp } from "lucide-react";
+
+export const description = "A bar chart";
+const chartData = [
+  { month: "Janeiro", received: 1245789 },
+  { month: "Fevereiro", received: 1984321 },
+  { month: "Março", received: 2765432 },
+  { month: "Abril", received: 1456789 },
+  { month: "Maio", received: 2234567 },
+  { month: "Junho", received: 1876543 },
+  { month: "Julho", received: 2987654 },
+  { month: "Agosto", received: 1567890 },
+  { month: "Setembro", received: 2678912 },
+  { month: "Outubro", received: 1789456 },
+  { month: "Novembro", received: 2345678 },
+  { month: "Dezembro", received: 2891234 },
+];
+const chartConfig = {
+  received: {
+    label: "Recebido",
+    color: "var(--primary)",
+  },
+};
+
+export default function ChartVerticalBar() {
+  return (
+    <Card className="@container/card p-5">
+      <CardHeader>
+        <CardTitle>Recebimentos por mês</CardTitle>
+        <CardDescription>
+          <span className="@[540px]/card:block">Do ano de 2026</span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Bar dataKey="received" fill="var(--primary)" radius={8} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
